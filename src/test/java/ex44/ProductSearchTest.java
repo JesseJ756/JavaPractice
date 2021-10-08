8a6f793dcfbc1506ce744258f5d3e1db2f874d65
jessejohnson1423@gmail.com <jessejohnson1423@gmail.com>
2021-10-07 22:38:43 -0400

    completed ex46 UML and created test files

---
+++ src/test/java/ex44/ProductSearchTest.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Jesse Johnson
+ */
+
+package ex44;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProductSearchTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/ex46/WordFrequencyFinderTest.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Jesse Johnson
+ */
+
+package ex46;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class WordFrequencyFinderTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
